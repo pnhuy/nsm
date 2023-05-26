ecb426115cf6eb6f74689d9010be2d643855bad2
Anthony Gatti <aagatti@stanford.edu>
2023-05-25 18:17:24 -0700

    Modularized deep_sdf network

--- GenerativeAnatomy/sdf/models/deep_sdf.py
+++ GenerativeAnatomy/sdf/models/deep_sdf.py
@@ -36,12 +36,13 @@ class Decoder(nn.Module):
         dims,
         n_objects=1,
         dropout=None,
-        dropout_prob=0.0,
-        norm_layers=(),
+        dropout_prob=0.2,
+        norm_layers=(), # DEPRECATED
+        latent_dropout=False, # DEPRECATED
         latent_in=(),
-        weight_norm=False,
+        weight_norm=True,
+        # batch_norm=False,
         xyz_in_all=None,
-        latent_dropout=False,
         activation="relu",  # "relu" or "sin"
         final_activation="tanh", #"sin", "linear"
         concat_latent_input=False,
@@ -63,178 +64,245 @@ class Decoder(nn.Module):
         """
         super(Decoder, self).__init__()
 
+        #DEPRECATED
+        self.norm_layers = norm_layers
+
         def make_sequence():
             return []
         
         self._activation_ = activation
         self._final_activation_ = final_activation
+        self.concat_latent_input = concat_latent_input
+        self.dims = [latent_size + 3] + dims
+        self.latent_in = latent_in
+        self.progressive_add_depth = progressive_add_depth
+        self.progressive_depth_params = progressive_depth_params
 
         #layers:
         # 0: input
         # 1 to N-hidden: NN
         # -1: output
         if n_objects == 1:
-            dims = [latent_size + 3] + dims + [1]
+            self.dims = self.dims + [1]
         else:
-            dims = [latent_size + 3] + dims + [n_objects]
+            self.dims = self.dims + [n_objects]        
 
-        self.num_layers = len(dims)
-        self.norm_layers = norm_layers
-        self.latent_in = latent_in
-        self.latent_dropout = latent_dropout
-        if self.latent_dropout:
-            self.lat_dp = nn.Dropout(0.2)
-
-        self.xyz_in_all = xyz_in_all
-        self.weight_norm = weight_norm
-
-        self.progressive_add_depth = progressive_add_depth
-        self.progressive_depth_params = progressive_depth_params
-
+        # self.xyz_in_all = xyz_in_all
+        # self.weight_norm = weight_norm
         # self.activation = activation
 
-        for layer in range(0, self.num_layers - 1):
-            if concat_latent_input == False:
-                in_dim = dims[layer]
-                if (layer + 1 in latent_in):
-                    out_dim = dims[layer + 1] - dims[0]
-                else:
-                    out_dim = dims[layer + 1]
-            elif concat_latent_input == True:
-                out_dim = dims[layer + 1]
-                if layer in latent_in:
-                    in_dim = dims[layer] + dims[0]
-                else:
-                    in_dim = dims[layer]
-            else:
-                in_dim = dims[layer]
-                out_dim = dims[layer + 1]
-            if self.xyz_in_all and layer != self.num_layers - 2:
-                #TODO: Should this also include logic for
-                # concat_latent_input? - right now it assumes not concat
-                out_dim -= 3
-
-            lin = nn.Linear(in_dim, out_dim)
-
-            # be sure to specially initialize layer weights 
-            # for the sine activation based network (Siren). 
-            if activation == 'sin':
-                with torch.no_grad():
-                    num_input = lin.weight.size(-1)
-                    if layer == 0:
-                        torch.nn.init.uniform_(lin.weight, -1 / num_input, 1 / num_input)
-                    else:
-                        torch.nn.init.uniform_(lin.weight, -np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
-
-            if weight_norm and layer in self.norm_layers:
-                setattr(
-                    self,
-                    "lin" + str(layer),
-                    nn.utils.weight_norm(lin),
-                )
-            else:
-                setattr(self, "lin" + str(layer), lin)
-
-            if (
-                (not weight_norm)
-                and self.norm_layers is not None
-                and layer in self.norm_layers
-            ):
-                setattr(self, "bn" + str(layer), nn.LayerNorm(out_dim))
+        self.layers = nn.ModuleList()
         
-        # SETUP ACTIVATION
-        if activation == "relu":
-            self.activation = nn.ReLU()
-        elif activation == "sin":
-            self.activation = Sine()
+        # Add the rest of the layers
+        for layer in range(len(self.dims)-1):
+            # get layer input and output dimensions
+            in_dim, out_dim = self.get_layer_dims(layer)
+            lin_layer = nn.Linear(in_dim, out_dim)
+            # initialize the weights - particularly for the sine activation
+            init_weights(module=lin_layer, activation=self._activation_, first_layer=layer==0)
+            # add weight norm if specified
+            if weight_norm is True and layer in self.norm_layers:
+                lin_layer = nn.utils.weight_norm(lin_layer)
+            self.layers.append(lin_layer)
+            
         
-        # SETUP FINAL ACTIVATION
-        if final_activation == "sin":
-            self.final_activation = Sine()
-        elif final_activation == "tanh":
-            self.final_activation = nn.Tanh()
-        elif final_activation == "linear":
-            self.final_activation = None
-        else:
-            raise ValueError("Invalid final activation")
-        
+        self.activation = get_activation(self._activation_)
+        self.final_activation = get_activation(self._final_activation_)
+        # # Add final activation
+        # if final_activation != "linear":
+        #     self.layers.append(get_activation(self._final_activation_))
 
         self.dropout_prob = dropout_prob
         self.dropout = dropout
         self.epoch = None
+            
+            # if self.xyz_in_all and layer != self.num_layers - 2:
+            #     #TODO: Should this also include logic for
+            #     # concat_latent_input? - right now it assumes not concat
+            #     out_dim -= 3
 
-    # input: N x (L+3)
-    def forward(self, input, epoch=None):
-        xyz = input[:, -3:]
+            # lin = nn.Linear(in_dim, out_dim)
 
-        if input.shape[1] > 3 and self.latent_dropout:
-            latent_vecs = input[:, :-3]
-            latent_vecs = F.dropout(latent_vecs, p=0.2, training=self.training)
-            x = torch.cat([latent_vecs, xyz], 1)
-        else:
-            x = input
+            # be sure to specially initialize layer weights 
+            # for the sine activation based network (Siren). 
+            # if activation == 'sin':
+            #     with torch.no_grad():
+            #         num_input = lin.weight.size(-1)
+            #         if layer == 0:
+            #             torch.nn.init.uniform_(lin.weight, -1 / num_input, 1 / num_input)
+            #         else:
+            #             torch.nn.init.uniform_(lin.weight, -np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
 
-        for layer in range(0, self.num_layers - 1):
-            lin = getattr(self, "lin" + str(layer))
+            # if weight_norm and layer in self.norm_layers:
+            #     setattr(
+            #         self,
+            #         "lin" + str(layer),
+            #         nn.utils.weight_norm(lin),
+            #     )
+            # else:
+            #     setattr(self, "lin" + str(layer), lin)
+            # if (
+            #     (not weight_norm)
+            #     and self.norm_layers is not None
+            #     and layer in self.norm_layers
+            # ):
+            #     setattr(self, "bn" + str(layer), nn.LayerNorm(out_dim))
+        
+        # # SETUP ACTIVATION
+        # if activation == "relu":
+        #     self.activation = nn.ReLU()
+        # elif activation == "sin":
+        #     self.activation = Sine()
+        
+
+       
+
+        # # SETUP FINAL ACTIVATION
+        # if final_activation == "sin":
+        #     self.final_activation = Sine()
+        # elif final_activation == "tanh":
+        #     self.final_activation = nn.Tanh()
+        # elif final_activation == "linear":
+        #     self.final_activation = None
+        # else:
+        #     raise ValueError("Invalid final activation")
+        
+
+        
+
+    def get_layer_dims(self, layer):
+        if self.concat_latent_input == False:
+            in_dim = self.dims[layer]
+            if (layer + 1 in self.latent_in):
+                out_dim = self.dims[layer + 1] - self.dims[0]
+            else:
+                out_dim = self.dims[layer + 1]
+        elif self.concat_latent_input == True:
+            out_dim = self.dims[layer + 1]
             if layer in self.latent_in:
-                xi = torch.cat([x, input], 1)
-            elif layer != 0 and self.xyz_in_all:
-                xi = torch.cat([x, xyz], 1)
+                in_dim = self.dims[layer] + self.dims[0]
+            else:
+                in_dim = self.dims[layer]
+        else:
+            in_dim = self.dims[layer]
+            out_dim = self.dims[layer + 1]
+
+        return in_dim, out_dim
+    
+    # input: N x (L+3)
+    def forward(self, input_, epoch=None):
+        # Assign the epoch in case needed (for progressive depth)
+        if epoch is not None:
+            self.epoch = epoch
+
+        xyz = input_[:, -3:]
+        x = input_            
+
+        for layer_idx, layer in enumerate(self.layers): #range(0, self.num_layers - 1):
+            if (layer_idx in self.latent_in):
+                xi = torch.cat([x, input_], 1)
+            # elif layer_idx != 0 and self.xyz_in_all:
+            #     xi = torch.cat([x, xyz], 1)
             else:
                 xi = x
             
-            if (self.progressive_add_depth is True) and (layer in self.progressive_depth_params['layers']):
-                # use this as a way to store the progress of the network so far.
-                # this way if we try to use the partly tuned model for inference, it will be able to 
-                # use the weights that have been tuned so far.
-                if epoch is not None:
-                    self.epoch = epoch
-                # progresive tuning of latter layers is from Curriculum DeepSDF
-                # code was adapted from:
-                #https://github.com/haidongz-usc/Curriculum-DeepSDF
-                start = self.progressive_depth_params['layers'][layer]['start_epoch']
-                warmup = self.progressive_depth_params['layers'][layer]['warmup_epochs']
-                end = start + warmup
-                if self.epoch < start:
-                    # before start... dont apply this block at all.
-                    # we are doing continue (vs break) because the 
-                    # final layer should still be applied. 
-                    continue
-                elif start < self.epoch < end:
-                    # during warmup... linearly phase this block in
-                    # https://github.com/haidongz-usc/Curriculum-DeepSDF/blob/ca216dda8edc6435139a6f657c45800791be94a7/networks/deep_sdf_decoder_train.py#L113
-                    new_weight = ((self.epoch - start)/warmup)
-                    new_weight = new_weight**2
-                    base_weight = 1 - new_weight
-                    # base_weight = ((end-self.epoch)/warmup)
-
-                    x_base = xi * base_weight
-                    x_new = lin(xi) * new_weight
-                    x = x_base + x_new
+            if ((self.progressive_add_depth is True) and (layer_idx in self.progressive_depth_params['layers'])):
+                if self.epoch >= self.progressive_depth_params['layers'][layer_idx]['start_epoch']:
+                    x = self.progressive_layer(xi, layer, layer_idx)     
                 else:
-                    # after start + warmup epochs just apply this block as a normal layer
-                    x = lin(xi)
+                    continue        
             else:
-                x = lin(xi)
+                x = layer(xi)
             
             # only apply normalization/ regular activation to 
             # hidden layers (not output)
-            if layer < self.num_layers - 2:
-                if (
-                    self.norm_layers is not None
-                    and layer in self.norm_layers
-                    and not self.weight_norm
-                    and (self._activation_ != "sin")
-                ):
-                    bn = getattr(self, "bn" + str(layer))
-                    x = bn(x)
-                
+            if layer_idx < len(self.layers) - 1:
+
                 x = self.activation(x)
 
-                if self.dropout is not None and layer in self.dropout:  #and (self._activation_ != "sin")
+                if self.dropout is not None and layer_idx in self.dropout:  #and (self._activation_ != "sin")
                     x = F.dropout(x, p=self.dropout_prob, training=self.training)
 
         if self.final_activation is not None:
             x = self.final_activation(x)
 
         return x
+    
+    def progressive_layer(self, xi, layer, layer_idx):
+        # use this as a way to store the progress of the network so far.
+        # this way if we try to use the partly tuned model for inference, it will be able to 
+        # use the weights that have been tuned so far.
+        
+        # progresive tuning of latter layers is from Curriculum DeepSDF
+        # code was adapted from:
+        #https://github.com/haidongz-usc/Curriculum-DeepSDF
+        start = self.progressive_depth_params['layers'][layer_idx]['start_epoch']
+        warmup = self.progressive_depth_params['layers'][layer_idx]['warmup_epochs']
+        end = start + warmup
+        if self.epoch < start:
+            raise exception("Epoch is before start of progressive depth")
+        elif start < self.epoch < end:
+            # during warmup... linearly phase this block in
+            # https://github.com/haidongz-usc/Curriculum-DeepSDF/blob/ca216dda8edc6435139a6f657c45800791be94a7/networks/deep_sdf_decoder_train.py#L113
+            new_weight = ((self.epoch - start)/warmup)
+            new_weight = new_weight**2
+            base_weight = 1 - new_weight
+            # base_weight = ((end-self.epoch)/warmup)
+
+            x_base = xi * base_weight
+            x_new = layer(xi) * new_weight
+            x = x_base + x_new
+        else:
+            # after start + warmup epochs just apply this block as a normal layer
+            x = layer(xi)
+        
+        return x
+
+def init_weights(module, activation, first_layer=False):
+    """
+    Initializes the weights of a linear layer based on the activation function.
+    """
+    if isinstance(module, nn.Linear):
+        num_input = module.weight.size(-1)
+        if activation == 'sin':
+            with torch.no_grad():
+                if first_layer is True:
+                    b = 1/num_input
+                elif first_layer is False:
+                    b = np.sqrt(6 / num_input) / 30
+
+                torch.nn.init.uniform_(module.weight, -b, b)
+
+def weight_norm_all(module):
+    """
+    Applies weight normalization to all linear layers in a PyTorch module.
+    """
+    def apply_weight_norm(module):
+        if isinstance(module, nn.Linear):
+            nn.utils.weight_norm(module)
+
+    module.apply(apply_weight_norm)
+
+def get_activation(activation):
+    if activation == 'relu':
+        return nn.ReLU()
+    elif activation == 'leaky_relu':
+        return nn.LeakyReLU()
+    elif activation == 'sigmoid':
+        return nn.Sigmoid()
+    elif activation == 'tanh':
+        return nn.Tanh()
+    elif activation == 'softplus':
+        return nn.Softplus()
+    elif activation == 'elu':
+        return nn.ELU()
+    elif activation == 'selu':
+        return nn.SELU()
+    elif activation == 'swish':
+        return nn.SiLU()
+    elif activation == 'sin':
+        return Sine()
+    elif activation == 'linear':
+        return None
+    else:
+        raise ValueError(f'Unknown activation function: {activation}')
